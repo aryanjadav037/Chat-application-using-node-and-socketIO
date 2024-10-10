f12f4fcd262265c620ecd240ca2e439fd1efb5a6
aryan <aryanjadav081103@gmail.com>
2024-10-10 12:51:06 +0530

    index.js

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,2 +1,20 @@
 //node server for handl socketIO connections
 
+const { Socket } = require('socket.io');
+
+const io=require('socket.io')(8000)
+
+const users = {};
+
+io.on('connection',Socket =>{
+    Socket.on('new-user-joined',name =>{
+        users[Socket.id]=name;
+        Socket.brodcast.emit('user-joined',name);
+        });
+
+    Socketocket.on('send',message =>{
+        Socket.brodcast.emit('receive',{message:message,name:users[Socket.id]});
+    });
+
+})
+
