aba430e21b597187f3a7e98f1fadefa4de6dafa6
aryan <aryanjadav081103@gmail.com>
2024-10-10 15:02:49 +0530

    last commit

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,20 +1,21 @@
 //node server for handl socketIO connections
-
-const { Socket } = require('socket.io');
-
-const io=require('socket.io')(8000)
+const io = require('socket.io')(8000)
 
 const users = {};
 
-io.on('connection',Socket =>{
-    Socket.on('new-user-joined',name =>{
-        users[Socket.id]=name;
-        Socket.brodcast.emit('user-joined',name);
+io.on('connection',socket =>{
+    socket.on('new-user-joined',name =>{
+        //console.log("New user",name);
+        users[socket.id]=name;
+        socket.broadcast.emit('user-joined',name);
         });
 
-    Socketocket.on('send',message =>{
-        Socket.brodcast.emit('receive',{message:message,name:users[Socket.id]});
+    socket.on('send',message =>{
+        socket.broadcast.emit('receive',{message:message,name:users[socket.id]})
     });
 
-})
-
+    socket.on('disconnect',message =>{
+        socket.broadcast.emit('left',users[socket.id]);
+        delete users[socket.id];
+    });
+});
